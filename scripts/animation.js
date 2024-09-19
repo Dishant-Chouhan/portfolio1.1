b878fa4699dcc08490ec2bbde36084fbefc6bc71
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2024-09-19 20:21:30 +0530

    page 1 without responsive ness created

---
+++ scripts/animation.js
@@ -0,0 +1,18 @@
+var tl = gsap.timeline();
+tl.from(".part1 h2", {
+    scale: 0,
+    duration: 1,
+    ease: "elastic.out(1, 0.5)",  // Elastic ease for a bubble-like effect
+    opacity: 0,
+});
+
+tl.from(".part1 .h1", {
+    y: 110,
+    duration: 1,
+});
+gsap.from("#comment", {
+    x: "-200%",
+});
+gsap.from("#experience", {
+    x: "200%",
+});
