8d3a4a0ecf122d28e0d46ab1059494c5b52399ef
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2024-09-19 19:35:40 +0530

    resposiveness added to the navbar

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,29 @@ document.addEventListener("DOMContentLoaded", function () {
         loader.style.opacity = '0';
         loader.style.visibility = 'hidden';
     });
+
+    const menuIcon = document.querySelector('.menuIcon i');
+    const menuIconContainer = document.querySelector('.menuIcon');
+    const menuTray = document.querySelector('.menuTray');
+
+    menuIcon.addEventListener('click', () => {
+        menuTray.classList.toggle('active');
+
+        if (menuTray.classList.contains('active')) {
+            menuIcon.classList.replace('ri-menu-3-line', 'ri-close-line');
+            menuIconContainer.style.backgroundColor = "#000000";
+        } else {
+            menuIcon.classList.replace('ri-close-line', 'ri-menu-3-line');
+            menuIconContainer.style.backgroundColor = "#171717";
+        }
+    });
+
+    const menuLinks = document.querySelectorAll('.menuTray a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuTray.classList.remove('active');
+            menuIcon.classList.replace('ri-close-line', 'ri-menu-3-line');
+            menuIconContainer.style.backgroundColor = "#171717";
+        });
+})
 })
