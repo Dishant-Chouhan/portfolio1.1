a04110b3afb4ec65cdc3d174a16be67ca8af6e8e
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2024-09-19 20:41:36 +0530

    responsiveness added to page 1

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,18 +1,22 @@
 var tl = gsap.timeline();
 tl.from(".part1 h2", {
     scale: 0,
-    duration: 1,
     ease: "elastic.out(1, 0.5)",  // Elastic ease for a bubble-like effect
     opacity: 0,
 });
 
 tl.from(".part1 .h1", {
     y: 110,
-    duration: 1,
 });
 gsap.from("#comment", {
     x: "-200%",
 });
 gsap.from("#experience", {
     x: "200%",
+});
+gsap.from(".circle", {
+    scale: 0,
+    ease: "elastic.out(1, 0.5)",  // Elastic ease for a bubble-like effect
+    opacity: 0,
+    duration:1
 });
